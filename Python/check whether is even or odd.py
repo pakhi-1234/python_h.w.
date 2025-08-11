9c9f739de0c2887dba92fda080b86913254c7cef
Adwiteya Das <anuasit33@gmail.com>
2025-08-11 15:34:45 +0530

    Add files via upload

---
+++ Python/check whether is even or odd.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a value:"))
+if(num%2==0)
+     print("Even value")
+else:
+    print("Odd")
---
+++ Python/find the largest of three number.py
@@ -0,0 +1,10 @@
+a=int(input("Enter 1st value:"))
+b=int(input("Enter 2nd value:"))
+c=int(input("Enter 3rd value:"))
+if(a>b & b>c):
+    print(a is the largest number)
+elif(b>a & b>c):
+    print(b is the largest number)
+else:
+    print(c is the largest number)
+    
